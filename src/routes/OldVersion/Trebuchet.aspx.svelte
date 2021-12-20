e56b0c7620d675458ed9c10a5b6a1b6d72904464
Peter Olson <peter.e.c.olson+github@gmail.com>
2021-12-20 11:05:57 -0600

    old version page

---
+++ src/routes/OldVersion/Trebuchet.aspx.svelte
@@ -0,0 +1,6 @@
+<script context="module">
+	// This page is added for backward compatibility to prevent broken links.
+	export async function load() {
+		return { status: 302, redirect: '/' };
+	}
+</script>
